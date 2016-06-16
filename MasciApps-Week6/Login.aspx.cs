2e9ea0201c5858072ac04375622dee6061375b95
Dan Masci <minimasci@yahoo.ca>
2016-06-16 11:00:46 -0400

    DefaultConnection and Login.aspx

--- MasciApps-Week6/Login.aspx.cs
+++ MasciApps-Week6/Login.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//Added - required for Identity
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace MasciApps_Week6
 {
     public partial class Login : System.Web.UI.Page
@@ -16,7 +21,29 @@ namespace MasciApps_Week6
 
         protected void LoginButton_Click(object sender, EventArgs e)
         {
+            // create new userStore and userManager
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
 
+            // retrieve user from db using credentials from login.aspx
+            var user = userManager.Find(UserNameTextBox.Text, PasswordTextBox.Text);
+
+            if (user != null)
+            {// if we have a match
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                // signin the authenticated user
+                authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = false }, userIdentity);
+
+                // redirect to MainMenu
+                Response.Redirect("~/MainMenu.aspx");
+            }
+            else
+            {
+                StatusLabel.Text = "Invalid Username or Password";
+                AlertFlash.Visible = true;
+            }
         }
     }
 }
---
+++ MasciApps-Week6/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MasciApps_Week6.Startup))]
+
+namespace MasciApps_Week6
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
